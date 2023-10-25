2cbc3190572a3004d5377ed2b93ac3402c8ffe44
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-10-24 19:17:37 -0500

    feat: add usersRouter

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import usersRouter from './routers/users.router.js'
 
 export const app = express()
 
 app.get('/', (req, res) => res.send('ok'))
+app.use('/users', usersRouter)
 
 app.listen(8080, () => console.log('Server Up!'))
